491d6bf41930f2a8f0fa6cf38879f87eddd62db1
Daniel Autenrieth <danielautenrieth@sinatra.fritz.box>
2016-10-07 11:24:55 +0200

    Initial Commit

---
+++ Animations/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Animations
+//
+//  Created by Daniel Autenrieth on 07/10/2016.
+//  Copyright © 2016 Brian Advent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
